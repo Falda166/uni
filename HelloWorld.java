7f1b9d17e3f22583eb15978bc5e090eef17bc7f5
Falda166 <haihappenfelix@gmail.com>
2025-09-24 19:35:31 +0200

    initial

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hallo Frau Mohnke ich habe keine Ahnung was ich schreiben kann :D");
+        System.out.println("Script funktioniert wie man sieht");
+        System.out.println("ist auch hoch komplex :D");
     }
 }
