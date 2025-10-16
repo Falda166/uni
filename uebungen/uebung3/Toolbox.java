fe4a96b07cbb597c8b0e6ce1468b2d3af9735735
Falda166 <haihappenfelix@gmail.com>
2025-10-16 13:18:02 +0200

    Add MethodsSandbox and Toolbox classes with basic methods; update coords and output image

---
+++ uebungen/uebung3/Toolbox.java
@@ -0,0 +1,23 @@
+package uebungen.uebung3;
+
+/**
+ * A toolbox with a collection of first Java-methods
+ * @author Janett Mohnke
+ * @version 1.0 2023
+ */
+public class Toolbox {
+    public static void printnums(int a, int b) {
+        System.out.println("Zahl 1: " + a);
+        System.out.println("Zahl 2: " + b);
+    }
+
+    public static void printfloats(float a, float b) {
+        System.out.println("Zahl 1: " + a);
+        System.out.println("Zahl 2: " + b);
+    }
+
+    public static void printstrings(String a, String b) {
+        System.out.println("String 1: " + a);
+        System.out.println("String 2: " + b);
+    }
+}
